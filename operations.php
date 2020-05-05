295f2978a600605a665ca92e5334a4eb957b98e0
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-05-05 13:56:24 -0300

    Types and variables

---
+++ operations.php
@@ -0,0 +1,18 @@
+<?php
+
+$plus = 2 + 2;
+$minus = 3 - 2;
+$multiplication = 2 * 2;
+$division = 4 / 2;
+$square = 2 ** 2;
+$rest = 10 % 3;
+$squareFoot = sqrt(4);
+
+
+echo $plus."\n";
+echo $minus."\n";
+echo $multiplication."\n";
+echo $division."\n";
+echo $square."\n";
+echo $rest."\n";
+echo $squareFoot."\n";
---
+++ primitiveType.php
@@ -0,0 +1,13 @@
+<?php
+
+$int = 21;
+$double = 100.30;
+$string = "Hello World";
+$boolean = $int === $double;
+
+
+echo gettype($int)."\n";
+echo gettype($double)."\n";
+echo gettype($string)."\n";
+echo gettype($boolean)."\n";
+
